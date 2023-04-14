bb5b8edfd4e54b99fc2f3bf61d27ac0bc4c86dd8
ampatte <ampatterson1@hotmail.com>
2023-04-14 14:58:40 -0400

    corrected typos

--- Main/routes/api/thoughtRoutes.js
+++ Main/routes/api/thoughtRoutes.js
@@ -29,7 +29,7 @@ router
 
 // **`/api/thoughts/:thoughtId/reactions/:reactionId`**
 router
-.route('/:thoughtId/reactions/:reaction/Id')
+.route('/:thoughtId/reactions/:reactionId')
 .delete(removeReaction);
 
 module.exports = router;
--- Main/routes/api/userRoutes.js
+++ Main/routes/api/userRoutes.js
@@ -24,13 +24,12 @@ router
 
 // **`/api/users/:userId/friends`**
 router
-.route('/:userId/Friends')
-.post(addFriend);
+.route('/:userId/friends')
+.post(addFriend)
 
 // **`/api/users/:userId/friends/:friendId`**
 router
-.route('/:userId/Friends/:Friend/Id')
+.route('/:userId/friends/:friendId')
 .delete(removeFriend);
-
 
 module.exports = router
